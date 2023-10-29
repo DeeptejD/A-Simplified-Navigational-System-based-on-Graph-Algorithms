9714d2743806d7d3b323ea19b2c081106543019e
joshuadlima <joshd11723@gmail.com>
2023-10-29 17:02:14 +0530

    added centrality and made the graph implementations same

--- dfs.py
+++ dfs.py
@@ -1,56 +1,32 @@
-# Python program to print DFS traversal for complete graph
-from collections import defaultdict
+from collections import deque
 
-
-# This class represents a directed graph using adjacency
-# list representation
 class Graph:
-    # Constructor
-    def __init__(self):
-        # default dictionary to store graph
-        self.graph = defaultdict(list)
+    def __init__(self, adjacency_list):
+        self.adjacency_list = adjacency_list
 
-    # function to add an edge to graph
-    def addEdge(self, u, v):
-        self.graph[u].append(v)
+    def get_neighbors(self, v):
+        return self.adjacency_list[v]
 
-    # A function used by DFS
-    def DFSUtil(self, v, visited):
-        # Mark the current node as visited and print it
-        visited[v] = True
-        print(v),
+    def dfs(self, node, visited):
+        if node not in visited:
+            print(node)
+            visited.add(node)
+            for neighbor, _ in self.get_neighbors(node):
+                self.dfs(neighbor, visited)
 
-        # Recur for all the vertices adjacent to
-        # this vertex
-        for i in self.graph[v]:
-            if visited[i] == False:
-                self.DFSUtil(i, visited)
-
-    # The function to do DFS traversal. It uses
-    # recursive DFSUtil()
-    def DFS(self):
-        V = len(self.graph)  # total vertices
-
-        # Mark all the vertices as not visited
-        visited = [False] * (V)
-
-        # Call the recursive helper function to print
-        # DFS traversal starting from all vertices one
-        # by one
-        for i in range(V):
-            if visited[i] == False:
-                self.DFSUtil(i, visited)
+    def dfs_traversal(self, start_node):
+        visited = set()
+        print("DFS Traversal:")
+        self.dfs(start_node, visited)
 
 
-# Driver code
-# Create a graph given in the above diagram
-g = Graph()
-g.addEdge(0, 1)
-g.addEdge(0, 2)
-g.addEdge(1, 2)
-g.addEdge(2, 0)
-g.addEdge(2, 3)
-g.addEdge(3, 3)
+adjacency_list = {
+    "A": [("B", 1), ("C", 3), ("D", 7)],
+    "B": [("D", 5)],
+    "C": [("D", 12)],
+    "D": [],
+}
 
-print("Following is Depth First Traversal")
-g.DFS()
+graph = Graph(adjacency_list)
+start_node = "A"
+graph.dfs_traversal(start_node)
