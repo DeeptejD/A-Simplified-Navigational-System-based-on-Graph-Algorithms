b8f17989b387164cce75fb4cd3c044b06b329ded
DeeptejD <deeptejdhauskar2003@gmail.com>
2023-11-02 19:13:14 +0530

    Running the server now directly redirects to the /plots page instead of the default /

--- GT/GT/urls.py
+++ GT/GT/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='plots/', permanent=False), name='index'),
     path("plots/", include("plots.urls")),
     path('admin/', admin.site.urls),
 ]
